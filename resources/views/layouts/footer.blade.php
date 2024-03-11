0efabb666b998a6469d633115c4a638d09a2fc0e
Perry1611 <perrysiregar0@gmail.com>
2024-03-11 17:06:12 +0700

    Tugas 2

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer footer-center p-4 bg-base-300 text-base-content mt-[100px]">
+    <aside>
+      <p>Copyright © 2024 - All right reserved by Perry Saputra Halim</p>
+    </aside>
+  </footer>
